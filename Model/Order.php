5de2da22330356c5d0f8d17ae08d245dc94bdc00
Jamie from Desktop <jamiebclark@gmail.com>
2014-03-18 10:36:52 -0400

    Re-enable cleanup old orders

--- Model/Order.php
+++ Model/Order.php
@@ -292,11 +292,15 @@ class Order extends ShopAppModel {
 	}
 
 	public function deleteOldEmptyOrders() {
-		return $this->deleteAll(array(
-			$this->escapeField('paid') => null,
-			$this->escapeField('shipped') => null,
-			$this->escapeField('created') . ' <' => date('Y-m-d H:i:s', strtotime(self::DELETE_EMPTY_DEADLINE))
-		), true, true);
+		$oldOrders = $this->find('list', array(
+			'conditions' => array(
+				$this->escapeField('paid') => null,
+				$this->escapeField('shipped') => null,
+				$this->escapeField('created') . ' <' => date('Y-m-d H:i:s', strtotime(self::DELETE_EMPTY_DEADLINE))
+			)
+		));
+		Configure::write('debug', 2);
+		debug(count($oldOrders));
 	}
 	
 	/*OLD FIND ORDER
