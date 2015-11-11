e55c73c84b89caebecf5ff052cbe1f7a6511c3a1
Jamie Desktop <jamiebclark@gmail.com>
2015-11-11 14:28:29 -0500

    Added search by Invoice ID to Invoice admin filter

--- Controller/InvoicesController.php
+++ Controller/InvoicesController.php
@@ -1,12 +1,12 @@
 <?php
 class InvoicesController extends ShopAppController {
-	var $name = 'Invoices';
+	public $name = 'Invoices';
 
-	var $components = array(
+	public $components = [
 		'FindFilter',
-	);
+	];
 
-	var $helpers = array(
+	public $helpers = [
 		'Layout.AddressBook',
 		'Layout.AddressBookForm',
 		'Layout.FormLayout',
@@ -14,25 +14,28 @@ class InvoicesController extends ShopAppController {
 		'Layout.Calendar', 
 		//'Layout.DateBuild', 
 		'Shop.Invoice'
-	);
+	];
 	
-	var $paginate = array('limit' => 50);
+	public $paginate = ['limit' => 50];
 	
 	public function _setFindFilters() {
 		$invoiceModelTypes = $this->Invoice->findModelTypes();
-		$invoiceModelTypes = array_combine($invoiceModelTypes, $invoiceModelTypes);
+		$invoiceModelTypes = ['' => ' --- Select a model --- '] + array_combine($invoiceModelTypes, $invoiceModelTypes);
 
-		$filters = array();
-		$filters['model'] = array('options' => $invoiceModelTypes);
-		$filters['model_id'] = array(
+		$filters = [];
+		$filters['invoice'] = ['label' => 'Invoice #', 'type' => 'text'];
+		$filters['model'] = ['options' => $invoiceModelTypes];
+		$filters['model_id'] = [
 			'label' => 'Model ID',
 			'type' => 'text',
-		);
+		];
 		return $filters;
 	}
 
-	public function _findFilterVal($key, $val, $query = array()) {
-		if ($key == 'model') {
+	public function _findFilterVal($key, $val, $query = []) {
+		if ($key == 'invoice') {
+			$query['conditions']['Invoice.id'] = $val;
+		} else if ($key == 'model') {
 			$query['conditions']['Invoice.model'] = $val;
 		} else if ($key == 'model_id') {
 			$query['conditions']['Invoice.model_id'] = $val;
@@ -40,24 +43,24 @@ class InvoicesController extends ShopAppController {
 		return $query;
 	}
 
-	function view($id) {
+	public function view($id) {
 		$this->Invoice->recursive = 1;
-		$invoice = $this->Invoice->find('first', array(
+		$invoice = $this->Invoice->find('first', [
 			'fields' => '*',
-			'postContain' => array('Order' => array('postContain' => 'OrderProduct')),
-			'conditions' => array('Invoice.id' => $id)
-		));
+			'postContain' => ['Order' => ['postContain' => 'OrderProduct']],
+			'conditions' => ['Invoice.id' => $id]
+		]);
 		$this->set(compact('invoice'));
 	}
 	
-	function admin_index() {
+	public function admin_index() {
 	//	$this->Invoice->fixDuplicates();
 		$this->Invoice->fixTotals();
 		
 		if (!empty($this->request->data['Invoice']['id'])) {
 			$invoice = $this->Invoice->findById($this->request->data['Invoice']['id']);
 			if (!empty($invoice)) {
-				$this->redirect(array('action' => 'view', $invoice['Invoice']['id']));
+				$this->redirect(['action' => 'view', $invoice['Invoice']['id']]);
 			} else {
 				$this->redirectMsg(true, 'Invoice #' . $this->request->data['Invoice']['id'] . ' Not Found', false);
 			}
@@ -67,41 +70,41 @@ class InvoicesController extends ShopAppController {
 		$this->set(compact('invoices'));
 	}
 	
-	function admin_view($id = null) {
+	public function admin_view($id = null) {
 		$this->Invoice->syncPaypal($id);
 		if (!empty($this->request->params['named']['notify'])) {
 			$msg = $this->Invoice->sendAdminPaidEmail($id) ? 'Email sent' : 'Error sending email';
-			$this->redirectMsg(array($id), $msg);
+			$this->redirectMsg([$id], $msg);
 		}
-		$this->FormData->findModel($id, null, array('contain' => array('PaypalPayment')));
+		$this->FormData->findModel($id, null, ['contain' => ['PaypalPayment']]);
 	}
 	
-	function admin_edit($id = null) {
-		$unsetValidates = array('email', 'addline1', 'city', 'state', 'zip', 'country', 'first_name', 'last_name');
+	public function admin_edit($id = null) {
+		$unsetValidates = ['email', 'addline1', 'city', 'state', 'zip', 'country', 'first_name', 'last_name'];
 		foreach ($unsetValidates as $field) {
 			unset($this->Invoice->validate[$field]);
 		}
 
 		$this->FormData->editData($id);
 		/*
-		if ($this->_saveData(null, null, array('validate' => false)) === null) {
+		if ($this->_saveData(null, null, ['validate' => false]) === null) {
 			$this->request->data = $this->Invoice->findById($id);
 			
 			//Preloads Bowlathon Pledge info
 			if (!empty($this->request->data['BowlathonPledge']['id']) && empty($this->request->data['Invoice']['first_name'])) {
-				$result = $this->Invoice->BowlathonPledge->BowlathonPledger->find('first', array(
-					'link' => array('BowlathonPledge'),
-					'conditions' => array(
+				$result = $this->Invoice->BowlathonPledge->BowlathonPledger->find('first', [
+					'link' => ['BowlathonPledge'],
+					'conditions' => [
 						'BowlathonPledge.id' => $this->request->data['BowlathonPledge']['id'],
-					)
-				));
+					]
+				]);
 				$result = $result['BowlathonPledger'];
 				if (empty($result['first_name']) && empty($result['last_name']) && !empty($result['name'])) {
 					$names = explode(' ', $result['name']);
 					$result['first_name'] = array_shift($names);
 					$result['last_name'] = implode(' ', $names);
 				}
-				$keys = array('user_id', 'first_name', 'last_name', 'addline1', 'addline2', 'city', 'state', 'zip', 'country', 'email', 'phone');
+				$keys = ['user_id', 'first_name', 'last_name', 'addline1', 'addline2', 'city', 'state', 'zip', 'country', 'email', 'phone'];
 				foreach ($keys as $key) {
 					if (empty($this->request->data['Invoice'][$key]) && !empty($result[$key])) {
 						$this->request->data['Invoice'][$key] = $result[$key];
@@ -112,22 +115,22 @@ class InvoicesController extends ShopAppController {
 		*/
 	}
 	
-	function admin_add() {
+	public function admin_add() {
 		$this->FormData->addData();
 	}
 	
-	function admin_delete($id = null) {
+	public function admin_delete($id = null) {
 		$this->FormData->deleteData($id);
 	}
 	
-	function admin_sync_paypal($id = null) {
+	public function admin_sync_paypal($id = null) {
 		$success = $this->Invoice->syncPaypal($id);
-		$redirect = array('action' => 'view', $id);
+		$redirect = ['action' => 'view', $id];
 
 		if ($success === false) {
 			$msg = 'Invoice not found';
 			$class = 'alert-error';
-			$redirect = array('action' => 'index');
+			$redirect = ['action' => 'index'];
 		} else if (empty($success)) {
 			$msg = 'PayPal payment not found';
 			$class = 'alert-warning';
@@ -139,24 +142,24 @@ class InvoicesController extends ShopAppController {
 		$this->redirect($redirect);
 	}
 	
-	function admin_resend_email($id = null) {
+	public function admin_resend_email($id = null) {
 		$this->FormData->findModel($id);
 		if ($success = $this->Invoice->sendAdminPaidEmail($id)) {
 			$msg = 'Email successfully sent';
 		} else {
 			$msg = 'There was an error sending email';
 		}
-		$this->redirectMsg(array('action' => 'view', $id), $msg, $success);
+		$this->redirectMsg(['action' => 'view', $id], $msg, $success);
 	}
 	
-	function admin_copy_payment($id = null) {
-		$invoice = $this->Invoice->find('first', array(
-			'fields' => array('*'),
-			'link' => array('Shop.PaypalPayment'),
-			'conditions' => array(
+	public function admin_copy_payment($id = null) {
+		$invoice = $this->Invoice->find('first', [
+			'fields' => ['*'],
+			'link' => ['Shop.PaypalPayment'],
+			'conditions' => [
 				'Invoice.id' => $id,
-			)
-		));
+			]
+		]);
 		if (!empty($invoice['PaypalPayment'])) {
 			if ($success = $this->Invoice->PaypalPayment->syncInvoice($invoice['PaypalPayment']['id'])) {
 				$msg = 'Successfully updated Invoice information';
@@ -167,10 +170,10 @@ class InvoicesController extends ShopAppController {
 		$this->redirectMsg(true, $msg, $success);
 	}
 	
-	function _setFormElements() {
+	public function _setFormElements() {
 		$this->set('states', $this->Invoice->State->selectList());
 		$this->set('countries', $this->Invoice->Country->selectList());
 		$this->set('invoicePaymentMethods', $this->Invoice->InvoicePaymentMethod->selectList());
-		//$this->set('models', array('' => ' --- No Model --- ') + $this->Invoice->syncedModels);
+		//$this->set('models', ['' => ' --- No Model --- '] + $this->Invoice->syncedModels);
 	}
 }
