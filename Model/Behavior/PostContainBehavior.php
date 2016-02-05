547d6657aa08a91557cb59c1bc7b2f19031447a3
Jamie Desktop <jamiebclark@gmail.com>
2016-02-04 19:05:22 -0500

    Updated PostContainBehavior method from ids to getModelIds

--- Model/Behavior/PostContainBehavior.php
+++ Model/Behavior/PostContainBehavior.php
@@ -1,40 +1,40 @@
 <?php
 class PostContainBehavior extends ModelBehavior {
-	var $name = 'PostContain';
+	public $name = 'PostContain';
 	
-	var $getPostContain = array();
+	public $getPostContain = [];
 	
 	//If a value is stored in the 'postContain' within options, store it until after the find
-	function beforeFind(Model $Model, $options = array()) {
+	public function beforeFind(Model $Model, $options = []) {
 		if (!empty($options['postContain'])) {
 			$this->getPostContain[$Model->alias] = $options['postContain'];
 			unset($options['postContain']);
 		}
-
 		return parent::beforeFind($Model, $options);
 	}
 	
 	//If a postContain value had been stored before the find, run it
-	function afterFind(Model $Model, $results, $primary = false) {
+	public function afterFind(Model $Model, $results, $primary = false) {
 		if (!empty($this->getPostContain[$Model->alias])) {
 			$results = $this->postContain($Model, $results, $this->getPostContain[$Model->alias]);
 			unset($this->getPostContain[$Model->alias]);
 		}
-		return parent::afterFInd($Model, $results, $primary);
+		return $results;
 	}
 	
 	/**
 	 * Inserts values into a result mimicking the "contain" feature, but after the result has been generated
 	 *
 	 **/
-	function postContain(Model $Model, $result, $modelNames) {
-		//debug(array($Model->alias, $result, $modelNames));
-		$ids = $this->ids($Model, $result);
+	public function postContain(Model $Model, $result, $modelNames) {
+		//debug([$Model->alias, $result, $modelNames]);
+		$ids = $this->getResultIds($Model, $result);
+
 		if (empty($ids)) {
 			return false;
 		}
 		if (!is_array($modelNames)) {
-			$modelNames = array($modelNames);
+			$modelNames = [$modelNames];
 		}
 		
 		//True if result of "find all", false if "find first"
@@ -49,39 +49,49 @@ class PostContainBehavior extends ModelBehavior {
 				//list($modelName, $options) = $modelName;
 			} else {
 				$modelName = $modelAlias;
-				$options = array();
+				$options = [];
 			}
-			
+
+			$LinkedModel = $Model->{$modelAlias};
+
 			if ($multiResult) {
 				$limit = count($result);
 			//	$limit = 40;
 				for ($i = 0; $i < $limit; $i++) {
-					$result[$i][$modelAlias] = array();
+					$result[$i][$modelAlias] = [];
 				}
 			} else {
-				$result[$modelAlias] = array();
+				$result[$modelAlias] = [];
 			}
 
-			$options = array_merge_recursive($options, array(
-				'fields' => array('*'),
+			$query = array_merge_recursive($options, [
+				'fields' => [
+					$LinkedModel->escapeField('*'),
+				],
 				'recursive' => -1,
-				'conditions' => array(
-					$Model->alias. '.' . $Model->primaryKey => $ids
-				)
-			));
-			//ddebug($options);
+				'conditions' => [
+					$Model->escapeField() => $ids
+				]
+			]);
+			$query['fields'][] = $Model->escapeField();
+
 			if ($modelName == $Model->alias) {
-				$modelResult = $Model->find('all', $options);
+				$modelResult = $Model->find('all', $query);
 			} else {
-				if (empty($options['link'])) {
-					$options['link'] = array();
-				} else if (!is_array($options['link'])) {
-					$options['link'] = array($options['link']);
+				if (empty($query['link'])) {
+					$query['link'] = [];
+				} else if (!is_array($query['link'])) {
+					$query['link'] = [$query['link']];
 				}
-				if (!array_search($Model->alias, $options['link'], true)) {
-					$options['link'][] = $Model->alias;
+				if (!array_search($Model->alias, $query['link'], true)) {
+					$query['link'][] = $Model->alias;
 				}
-				$modelResult = $Model->{$modelName}->find('all', $options);
+				$query['group'] = [$Model->escapeField(), $LinkedModel->escapeField()];
+				
+				if (empty($Model->{$modelName})) {
+					throw new Exception('Could not find ' . $modelName);
+				}
+				$modelResult = $LinkedModel->find('all', $query);	
 			}
 			
 			if (!empty($modelResult)) {
@@ -109,24 +119,18 @@ class PostContainBehavior extends ModelBehavior {
 		return $result;
 	}
 	
-	/**
-	 * Returns the primaryKeys of a found result
-	 *
-	 **/
-	function ids(Model $Model, $result) {
-		$ids = array();
+/**
+ * Returns the primaryKeys of a found result
+ *
+ **/
+	public function getResultIds(Model $Model, $result) {
+		$ids = [];
 		if (isset($result[$Model->alias][$Model->primaryKey])) {
 			return $result[$Model->alias][$Model->primaryKey];
 		}
 		if (empty($result) || !is_array($result)) {
-			return array();
+			return [];
 		}
-		
-		foreach ($result as $row) {
-			if (!empty($row[$Model->alias][$Model->primaryKey])) {
-				$ids[] = $row[$Model->alias][$Model->primaryKey];
-			}
-		}
-		return $ids;
+		return Hash::extract($result, '{n}.' . $Model->alias . '.' . $Model->primaryKey);
 	}
 }
